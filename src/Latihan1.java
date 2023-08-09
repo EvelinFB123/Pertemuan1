c7aea0f6caaefc37fb0170dbcfd4bba63a481157
EvelinFB123 <c14230100@john.petra.ac.id>
2023-08-09 14:24:24 +0700

    init

---
+++ src/Latihan1.java
@@ -0,0 +1,13 @@
+public class Latihan1 {
+    public static void main(String[] args) {
+        System.out.println("---Data Mahasiswa---");
+        System.out.println("Nama : Evelin Felicia Buntaran");
+        System.out.println("NRP : c14230100");
+        System.out.println("Mata Kuliah : Algoritma dan Pemrograman");
+        System.out.println("Kelas : B");
+        System.out.println("Tahun : 2023/2024");
+        System.out.println("Semester : Gasal");
+        System.out.println("---------------");
+
+    }
+}
---
+++ src/Pertemuan1.java
@@ -0,0 +1,8 @@
+public class Pertemuan1 {
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+        System.out.println("Hi");
+
+        System.out.println("Test");
+    }
+}
